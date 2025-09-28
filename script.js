81753ef99da89e0f80dc2e56ed1aba9cbeb19786
Lobsang Yungdung <[10lobsangyungdung10@gmail.com]>
2025-09-28 14:43:55 +0530

    Display only the last book in myLibrary

--- script.js
+++ script.js
@@ -16,8 +16,9 @@ function addBookToLibrary(title, author, pages, read){
 
 const container = document.querySelector(".container");
 
-function displayBooks(){
-    myLibrary.forEach((item) => {
+let displayBooks = function(){
+    myLibrary.forEach((item,i) => {
+        if(i == myLibrary.length-1){
         //console.log(item);
         const card = document.createElement("div");
         card.classList.add("book-card");
@@ -27,21 +28,32 @@ function displayBooks(){
         Pages:  ${item.pages}<br><br>
         Read:   ${item.read}`;
         container.appendChild(card);
+        }
     })
 }
 
 addBookToLibrary("Harry Potter", "J.K Rowling", 236, "Read");
+displayBooks();
 addBookToLibrary("Naruto", "Masashi Kishimoto", 2391, "Read");
 addBookToLibrary("One piece", "Oda", 6891, "Read");
 displayBooks();
 
 
-/*myLibrary.forEach((item) => {
-    console.log(item);
-    const card = document.createElement("div");
-    card.classList.add("book-card");
-    card.innerHTML = `
-    Title: ${item.title}<br>
-    Author: ${item.author}`;
-    container.appendChild(card);
-})*/
+let addButton = document.querySelector(".addBook");
+let dialog = document.querySelector("dialog");
+
+addButton.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+/*let submit = document.querySelector(".submit");
+let input = document.querySelector("input");
+
+
+submit.addEventListener("click", createBook);
+
+let createBook = function(){
+    addBookToLibrary();
+    displayBooks();
+}*/
+
